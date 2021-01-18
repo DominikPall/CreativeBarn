ccbdc7f92143ef8b92cbc74982c42031eb7621d2
Dominik Páll <Dominik.Pall.Eu@gmail.com>
2021-01-18 17:23:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   include 'nav&foot/header.php';
 ?>
         <title>Creative Barn Shop</title>
-        <link rel="stylesheet" href="../css/index.css">
+        <link rel="stylesheet" href="css/index.css">
       </head>
      
       <?php 
